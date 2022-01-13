e4327666406249af77d163613ca56b368135f535
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-01-13 13:33:36 -0600

    additional cmdline information for the user

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from pythonping import ping
 
 ##### REMOVE AFTER COMPLETION ####
+##### USED FOR CODE WRITING AND DEBUGGING PURPOSES #####
 import os
 import traceback
 
-md = "ping_record.md"
+md = "ping_results.md"
 
 if os.path.exists(md):
     os.remove(md)
 else:
-    print("File does not exist")
+    print("\nFile: " + md + " does not exist. No file deleted")
 #################################
 
 ## Open File that contains IPs, DNS Servers, and Domain Names
@@ -17,19 +18,18 @@ ping_file = open("./to_ping.txt", "r")
 
 to_ping = []
 for line in ping_file:
-    line = line[:-1]
+    line = line[:-1] # removes newline character from ping list
     to_ping.append(line)
-print(to_ping)
 try: 
     ping_record = open(md, "w")
-    ping_record.write("# Ping results from: "+ ping_file.name + "\n")
+    ping_record.write("# Ping results from: "+ ping_file.name + "\n") # creates the title of the .md file
 
-    print("Pinging items in " + ping_file.name)
+    print("\nPinging " + str(len(to_ping)) + " items in " + ping_file.name)
 
     for i in range(len(to_ping)):
         print("\n" + str(i + 1) + ": " + to_ping[i]) 
 
-        ping_record.write("\n## " + to_ping[i] + "\n")
+        ping_record.write("\n## " + to_ping[i] + "\n") # heading for a specific ping
         message = str(ping(to_ping[i], verbose=True))
         message = message.replace("\r", "")
 
@@ -52,22 +52,23 @@ altered_md_lines = []
 try:
     markdown_file = open(md, "r")
     md_lines = markdown_file.readlines()
+
+    ## Creates a .md list out of the ping results ##
     for line in  md_lines:
         if line[0] == "R":
             if "min/avg/max" in line:
-                line = "\t- " + line
+                line = "\t- " + line # additional list indentation for Roung Trip results
             else:
                 line = "- " + line
             altered_md_lines.append(line)
         else:
             altered_md_lines.append(line)
-    print(altered_md_lines)
 except:
     traceback.print_exc()
 finally:
     markdown_file.flush()
     markdown_file.close()
-
+## Applies final changs to the ping results ##
 try:
     final_md_file = open(md, "w")
     for line in altered_md_lines:
