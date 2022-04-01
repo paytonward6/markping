e0c1c2629f81ca376b0e35cf1f3705e7861104ac
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-04-01 13:17:03 -0500

    put even more of the code within methods

--- src/markping.py
+++ src/markping.py
@@ -5,6 +5,7 @@ import traceback
 import subprocess
 
 class markping:
+
     def __init__(self, ping_count, ping_wait_duration):
         markping.ping_count = ping_count
         markping.ping_wait_duration = ping_wait_duration
@@ -25,6 +26,32 @@ class markping:
         else:
             print("\nFile: " + markdown_file + " will be created since it does not exist.")
 
+    def ping_command(to_ping):
+        return subprocess.run(["ping",  "-c", str(markping.ping_count), "-i", str(markping.ping_wait_duration), to_ping],
+                                                  capture_output=True)
+    def write_ping_result(ping_result, ping_record_file):
+            stdout = ping_result.stdout.decode('UTF-8')
+            stdout = stdout.split("\n")
+            stderr = ping_result.stderr.decode('UTF-8')
+            
+            if "Unknown host" in stderr:
+                ping_record_file.write(stderr)
+            else:
+                for output in stdout:
+                    ping_record_file.write(output)
+                    ping_record_file.write("\n")
+
+    def write_results(ping_file, markdown_file, ping_list):
+        with open(markdown_file, "w") as ping_record:
+            ping_record.write("# Ping results from: "+ ping_file.name + "\n") # creates the title of the .md file
+
+            print("\nPinging " + str(len(ping_list)) + " items in " + ping_file.name)
+            
+            for i in tqdm(range(len(ping_list))):
+                    ping_record.write("\n## " + ping_list[i] + "\n") # heading for a specific ping
+                    result = markping.ping_command(ping_list[i])
+                    markping.write_ping_result(result, ping_record)
+
     def ping_list(ping_path, markdown_file):
         ## Open File that contains IPs, DNS Servers, and Domain Names
         with open(ping_path, "r") as ping_file:
@@ -33,27 +60,29 @@ class markping:
             for line in ping_file:
                 line = line[:-1] # removes newline character from ping list
                 to_ping.append(line)
-            with open(markdown_file, "w") as ping_record:
-                ping_record.write("# Ping results from: "+ ping_file.name + "\n") # creates the title of the .md file
+            markping.write_results(ping_file, markdown_file, to_ping)
 
-                print("\nPinging " + str(len(to_ping)) + " items in " + ping_file.name)
-                
-                for i in tqdm(range(len(to_ping))):
-                        ping_record.write("\n## " + to_ping[i] + "\n") # heading for a specific ping
-                        result = subprocess.run(["ping",  "-c", str(markping.ping_count), "-i", str(markping.ping_wait_duration), to_ping[i]],
-                                                      capture_output=True)
-                        stdout = result.stdout.decode('UTF-8')
-                        stdout = stdout.split("\n")
-                        stderr = result.stderr.decode('UTF-8')
-                        
-                        if "Unknown host" in stderr:
-                            ping_record.write(stderr)
-                        else:
-                            for output in stdout:
-                                ping_record.write(output)
-                                ping_record.write("\n")
+    def results_to_markdown(markdown_lines, line):
+            if line[0] == "#" or line[0] == "P":
+                markdown_lines.append(line)
+            elif line == '\n' or "statistics" in line:
+                pass
+            elif line[0] == str(markping.ping_count) or line[0] == 'r':
+                line = "\t- " + line # additional list indentation for Roung Trip results
+                markdown_lines.append(line)
+            elif "icmp_seq" or "Unknown host" in line:
+                markdown_lines.append("- " + line)
 
-
+    def format_results(md):
+        markdown_lines = []
+        with open(md, "r") as markdown_file:
+            raw_lines = markdown_file.readlines()
+            ## Creates a .md list out of the ping results ##
+            for line in raw_lines:
+                markping.results_to_markdown(markdown_lines, line)  ## Applies final changs to the ping results ##
+        with open(md, "w") as final_md_file:
+            for line in markdown_lines:
+                final_md_file.write(line)
 
     def run(self):
         md = ""
@@ -64,47 +93,10 @@ class markping:
         else:
             ping_filepath = sys.argv[1]
             md = sys.argv[2]
-    
+
         markping.initial_prompt(md)
         markping.ping_list(ping_filepath, md)
-
-
-
-
-        ##########################################
-        altered_md_lines = []
-        try:
-            markdown_file = open(md, "r")
-            md_lines = markdown_file.readlines()
-
-            ## Creates a .md list out of the ping results ##
-            for line in md_lines:
-                if line[0] == "#" or line[0] == "P":
-                    altered_md_lines.append(line)
-                elif line == '\n' or "statistics" in line:
-                    continue
-                elif line[0] == str(markping.ping_count) or line[0] == 'r':
-                    line = "\t- " + line # additional list indentation for Roung Trip results
-                    altered_md_lines.append(line)
-                elif "icmp_seq" or "Unknown host" in line:
-                    altered_md_lines.append("- " + line)
-        except:
-            traceback.print_exc()
-        finally:
-            markdown_file.flush()
-            markdown_file.close()
-
-        ## Applies final changs to the ping results ##
-        try:
-            final_md_file = open(md, "w")
-            for line in altered_md_lines:
-                final_md_file.write(line)
-        except:
-            traceback.print_exc()
-        finally:
-            final_md_file.flush()
-            final_md_file.close()
-
+        markping.format_results(md)
 
 if __name__ == "__main__":
     count = 4
